d31d323b6953bc38b9c514efd9c6bc691233167e
Florian Cassayre <florian.cassayre@gmail.com>
2019-12-04 13:20:16 +0100

    Improved solution 4 (bis).

--- src/main/scala/adventofcode/solutions/Day04.scala
+++ src/main/scala/adventofcode/solutions/Day04.scala
@@ -10,7 +10,7 @@ object Day04 extends Day(4) {
 
   val running = range.map(_.toString.toSeq)
     .filter(s => s == s.sorted)
-    .map(Seq.unfold(_)(s => s.headOption.map(h => s.span(_ == h))).map(_.size))
+    .map(_.groupBy(identity).values.map(_.size))
 
   override def solutionA = running.count(_.exists(_ >= 2))
 
